1ebfd287b98fa9f470ce002a64059c477207cc4f
alexis Algoud <alexis@atm-consulting.fr>
2017-05-16 11:14:09 +0200

    fix upload file & preview

--- documentConfidentiel.php
+++ documentConfidentiel.php
@@ -14,17 +14,17 @@
 	$langs->load('other');
 	
 	$PDOdb=new TPDOdb;
-	$ressource=new TRH_ressource;
+	$object=new TRH_ressource;
 	
 	if(isset($_REQUEST['id'])) {
-		$ressource->load($PDOdb, $_REQUEST['id']);
-		_fiche($PDOdb, $ressource);
+		$object->load($PDOdb, $_REQUEST['id']);
+		_fiche($PDOdb, $object);
 	}
 	
 	$PDOdb->close();
 	llxFooter();
 	
-	function _fiche(&$PDOdb, &$ressource) {
+	function _fiche(&$PDOdb, &$object) {
 		global $db,$user,$conf,$langs;
 		llxHeader('','Fichiers confidentiels');
 		$dir_base = DOL_DATA_ROOT.'/ressource/';
@@ -40,49 +40,13 @@
 		$html = new Form($db);
 		$formfile = new FormFile($db);
 		
-		if ($_REQUEST["sendit"])
-		{
-			$upload_dir = $upload_dir_base.dol_sanitizeFileName($ressource->getId());
+		$upload_dir = $upload_dir_base.dol_sanitizeFileName($object->getId());
 		
-			if (dol_mkdir($upload_dir) >= 0)
-			{
-				
-				$resupload = dol_move_uploaded_file($_FILES['userfile']['tmp_name'], $upload_dir . "/" . $_FILES['userfile']['name'],0,0,$_FILES['userfile']['error']);
-				
-		        if (is_numeric($resupload) && $resupload > 0)
-				{
-					$message = $langs->trans("FileTransferComplete");
-		            $error = false;
-				}
-				else
-				{
-					$langs->load("errors");
+		/*
+		 * Actions
+		 */
+		include_once DOL_DOCUMENT_ROOT . '/core/actions_linkedfiles.inc.php';
 		
-					if ($resupload < 0)	// Unknown error
-					{
-						$message = $langs->trans("ErrorFileNotUploaded");
-					}
-					else if (preg_match('/ErrorFileIsInfectedWithAVirus/',$resupload))	// Files infected by a virus
-					{
-						$message = $langs->trans("ErrorFileIsInfectedWithAVirus");
-					}
-					else	// Known error
-					{
-						$message = $langs->trans($resupload);
-					}
-				}
-			}
-		
-		}
-		
-		// Delete
-		if ($action == 'confirm_deletefile' && $confirm == 'yes')
-		{
-			$file = $dir_base . '/' . $_REQUEST['urlfile'];
-			dol_delete_file( $file, 0, 0, 0, 'FILE_DELETE', $object);
-		
-			$message = $langs->trans("FileHasBeenRemoved");
-		}
 		
 		// Get all files
 		$sortfield  = GETPOST("sortfield", 'alpha');
@@ -102,7 +66,7 @@
 		if (!$sortfield) $sortfield = "name";
 		
 		
-		$upload_dir = $upload_dir_base.dol_sanitizeFileName($ressource->getId());
+		$upload_dir = $upload_dir_base.dol_sanitizeFileName($object->getId());
 		
 		$filearray = dol_dir_list($upload_dir, "files", 0, '', '\.meta$', $sortfield, (strtolower($sortorder) == 'desc' ? SORT_DESC : SORT_ASC), 1);
 		$totalsize = 0;
@@ -113,25 +77,25 @@
 		
 		if ($action == 'delete')
 		{
-			$formconfirm = $html->formconfirm($_SERVER["PHP_SELF"].'?id='.$ressource->getId().'&urlfile='.urldecode($_REQUEST['urlfile']), $langs->trans('DeleteFile'), $langs->trans('ConfirmDeleteFile'), 'confirm_deletefile', '', 0, 0);
+			$formconfirm = $html->formconfirm($_SERVER["PHP_SELF"].'?id='.$object->getId().'&urlfile='.urldecode($_REQUEST['urlfile']), $langs->trans('DeleteFile'), $langs->trans('ConfirmDeleteFile'), 'confirm_deletefile', '', 0, 0);
 		}
 		
 		$can_upload = 1;
 		
-		echo dol_get_fiche_head(ressourcePrepareHead($ressource, 'ressource'), 'documentConfidentiel', 'Ressource');
+		echo dol_get_fiche_head(ressourcePrepareHead($object, 'ressource'), 'documentConfidentiel', 'Ressource');
 		
 		echo ($message ? dol_htmloutput_mesg($message, '', ($error ? 'error' : 'ok'), 0) : '');
 
 		echo ($formconfirm ? $formconfirm : '');
-		printLibelle($ressource);
+		printLibelle($object);
 		if($user->rights->ressource->ressource->uploadFilesRestricted){
-			$formfile->form_attach_new_file($_SERVER["PHP_SELF"].'?id='.$ressource->getId(), '', 0, 0, $can_upload);
-			$formfile->list_of_documents($filearray, $ressource, 'ressource', '&id='.$ressource->getId(),0,'ressource_restricted/'.$ressource->getId().'/',1);
+			$formfile->form_attach_new_file($_SERVER["PHP_SELF"].'?id='.$object->getId(), '', 0, 0, $can_upload);
+			$formfile->list_of_documents($filearray, $object, 'ressource', '&id='.$object->getId(),0,'ressource_restricted/'.$object->getId().'/',1);
 		}else{
-			$formfile->list_of_documents($filearray, $ressource, 'ressource', '&id='.$ressource->getId(),0,'ressource_restricted/'.$ressource->getId().'/',0);
+			$formfile->list_of_documents($filearray, $object, 'ressource', '&id='.$object->getId(),0,'ressource_restricted/'.$object->getId().'/',0);
 		}
 		
-		?><div style="clear:both"></div><?
+		?><div style="clear:both"></div><?php
 		
 		dol_fiche_end();
 		llxFooter();
